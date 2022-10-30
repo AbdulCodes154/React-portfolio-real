c9e1844ce203405f366d4f9d5d3f720befcbceed
Mohammed Abdul Qahhar <abdulqahhar@Mohammeds-MacBook-Pro.local>
2022-10-30 15:00:11 +0000

    added projects to portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -12,6 +12,27 @@ const Portfolio = () => {
           </h1>
         </div>
       </div>
+
+      <div className="container hex-colors">
+          <a className='hex-colors-link' target="_blank" rel="noreferrer" href='https://abdulcodes154.github.io/Hex-Colors/'>Hex colors
+          </a>
+      </div>
+
+      <div className="container vue3-todo">
+          <a className='vue3-todo-link' target="_blank" rel="noreferrer" href='https://thunderous-choux-e8991d.netlify.app/'>Vue todo app
+          </a>
+      </div>
+
+      <div className="container tic">
+          <a className='tic-link' target="_blank" rel="noreferrer" href='https://friendly-duckanoo-154524.netlify.app/'>Tic tac toe
+          </a>
+      </div>
+
+      <p className="hex-p">Learned how to create a button which changes the color of the page.</p>
+      <p className="vue-p">Learned how to create a simple todo app.</p>
+      <div className="tic-c">
+          <p className="tic-p">Learned how to use Javascript/React to create a tic tac toe game. This helped improve my problem solving skills.</p>
+      </div>
       <Loader type="pacman" />
     </>
   )
